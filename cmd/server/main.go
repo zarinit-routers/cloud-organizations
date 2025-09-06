dce4d25bbf31e1fcbe6b397398725424de37ca60
Katy248 <petrovanton247@gmail.com>
2025-09-06 16:15:13 +0300

    Remove hardcoded port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"github.com/charmbracelet/log"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"github.com/zarinit-routers/cloud-organizaions/handlers/node"
 	"github.com/zarinit-routers/cloud-organizaions/middleware/implementation"
 	"github.com/zarinit-routers/middleware/auth"
 )
 
+func init() {
+	viper.AutomaticEnv()
+}
 func main() {
 
 	server := gin.Default()
@@ -25,6 +30,17 @@ func main() {
 
 	api.POST("/authorize-node", node.AuthorizeNode())
 
-	log.Warn("Port 8060 is hardcoded, remove this ASAP")
-	server.Run(":8060")
+	server.Run(getAddress())
+}
+
+const DefaultPort = 8060
+
+func getPort() int {
+	viper.SetDefault("port", DefaultPort)
+	port := viper.GetInt("port")
+	return port
+}
+
+func getAddress() string {
+	return fmt.Sprintf(":%d", getPort())
 }
