6188e71b2dfccea6465f188e3e10d2df1bdbd8ef
Katy248 <petrovanton247@gmail.com>
2025-09-14 18:15:25 +0300

    Fix migration

--- migrations/001-migration.sql
+++ migrations/001-migration.sql
@@ -4,15 +4,15 @@ CREATE TABLE
         id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
         name VARCHAR(255) NOT NULL,
         passphrase VARCHAR(255),
-        created_at TIMESTAMPZ NOT NULL,
-        updated_at TIMESTAMPZ
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP
     );
 
 CREATE TABLE
     IF NOT EXISTS members (
         organization_id uuid REFERENCES organizations (id) ON DELETE CASCADE,
         user_id uuid NOT NULL,
-        created_at TIMESTAMPZ NOT NULL
+        created_at TIMESTAMP NOT NULL
     );
 
 -- +migrate Down
